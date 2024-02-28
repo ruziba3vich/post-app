3ea8e51a2f2e1520d5d7ef917ceb129d76c8dd85
Dostonbek <rruzibayevich@gmail.com>
2024-02-28 15:36:10 +0500

    Spring boot RESTful API

---
+++ post-app/src/test/java/com/prodonik/post/PostAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prodonik.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
